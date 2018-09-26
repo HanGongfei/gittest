d5997036cc989ece9a3350b86aaaf651c3752e39
HanGongfei <729951342@qq.com>
2018-09-26 16:50:43 +0800

    增加一个head.php文件

---
+++ head.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>班级信息录入</title>
+	<link rel="stylesheet" type="text/css" href="http://g.alicdn.com/sj/dpl/1.5.1/css/sui.min.css">
+	<link rel="stylesheet" type="text/css" href="http://g.alicdn.com/sj/dpl/1.5.1/css/sui-append.min.css">
+	<link rel="stylesheet" type="text/css" href="css/default.css">	
+<style type="text/css">
+	.my-head{
+		width:
+	}
+	.my-head h1{
+	
+	}
+	.userinfo{
+		position: absolute;
+		width: 200px;
+		height: 25px;
+		line-height: 25px;
+		top:40px;
+		right:150px;
+		font-size: 12px;
+	}
+	.userinfo span{
+		color: red;
+		font-weight:bold;
+	}
+</style>
+</head>
+<body>
+	<div class="sui-container">
+		<div class="my-head">
+			<h1>候选人投票系统1.0</h1>
+		</div>
